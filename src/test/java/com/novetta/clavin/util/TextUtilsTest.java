af1932eed67ea1509efda70cf8cc296abf5c3334
Jeremy Glesner <jglesner@novetta.com>
2020-11-19 18:10:58 -0500

    attempted fix

--- src/test/java/com/novetta/clavin/util/TextUtilsTest.java
+++ src/test/java/com/novetta/clavin/util/TextUtilsTest.java
@@ -56,7 +56,7 @@ public class TextUtilsTest {
         fileString.replaceAll("\\n|\\r\\n", System.getProperty("line.separator"));
         
         String expectedString = "Line1 word2\nLine2 word2 word3\nLine3";
-        expectedString.replaceAll("\\n|\\r\\n|\\n\\r", System.lineSeparator());
+        expectedString.replaceAll("\\n|\\r\\n|\\n\\r|\\r", System.lineSeparator());
        
         assertEquals("wrong output from fileToString", expectedString, fileString);
     }
